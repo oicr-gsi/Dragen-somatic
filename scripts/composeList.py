643f6fe6819510ee37aeb9e5dc57faa2fee07d5a
pruzanov <pruzanov@oicr.on.ca>
2025-02-04 09:47:20 -0500

    Adding scripts to replace in-line Python code

---
+++ scripts/composeList.py
@@ -0,0 +1,32 @@
+"""
+   list composing function to use inside dragenSomatic or dragenGermline workflows
+"""
+
+import json
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Parser for inputs to the script')
+parser.add_argument('-i', '--input', help='input json string', required=True)
+args = parser.parse_args()
+
+'''Below are the lines to use in the wdl'''
+inputData = json.loads(args.input)
+
+try:
+    myPattern = r'\S+?\:\S+'
+    rgs = re.findall(myPattern, inputData['readGroup'])
+    for rgroup in rgs:
+        if rgroup.startswith("ID:"):
+            RGID = rgroup.split(":")[1]
+            Lane = rgroup.split("_")[-2]
+        if rgroup.startswith("SM:"):
+            RGSM = rgroup.split(":")[1]
+        if rgroup.startswith("LB:"):
+            RGLB = rgroup.split(":")[1]
+    fastqR1 = inputData['fastqR1']
+    fastqR2 = inputData['fastqR2']
+    myResult = ",".join([RGID, RGSM, RGLB, Lane, fastqR1, fastqR2])
+    print(myResult)
+except:
+    print("Error parsing string")
---
+++ scripts/writeFile.py
@@ -0,0 +1,21 @@
+"""
+   Given an array of strings, write into a file formatted
+   for dragenSomatic and dragenGermline
+"""
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Parser for inputs to the script')
+parser.add_argument('-l', '--lanes', help='input strings', required=True)
+parser.add_argument('-o', '--output', help='output file', required=True)
+args = parser.parse_args()
+
+inLines = re.split(";", args.lanes) if args.lanes else []
+linesToPrint = ["RGID,RGSM,RGLB,Lane,Read1File,Read2File\n"]
+for inputString in inLines:
+    inputString.rstrip()
+    if not inputString.startswith("Error"):
+        linesToPrint.append(inputString + "\n")
+with open(args.output, "w") as tl:
+    tl.writelines(linesToPrint)
+tl.close()
